887eb9ffec268c489f04805ac98e65f0b8efb39e
Rhythrosa Labs <112411200+RhythrosaLabs@users.noreply.github.com>
2024-10-31 14:52:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ import base64
 # Sidebar for API keys
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
 
 # Titles for each model's capabilities
 st.title("🌀 Unified AI Chat Interface")
